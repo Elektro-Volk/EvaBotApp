0d19a73f46876cf126c9d33eec6ed58ddbb673be
root <root@ih538121.vds.myihor.ru>
2019-04-26 04:47:03 +0300

    Updates

---
+++ precache-manifest.dfddfb903cf077bf0836d8e46ff2bbc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3449b2891346eba686a1",
+    "url": "/EvaBotApp/static/css/main.1a693a22.chunk.css"
+  },
+  {
+    "revision": "3449b2891346eba686a1",
+    "url": "/EvaBotApp/static/js/main.e8ced18b.chunk.js"
+  },
+  {
+    "revision": "f55d19da24accba29ee4",
+    "url": "/EvaBotApp/static/js/runtime~main.d8387a40.js"
+  },
+  {
+    "revision": "92f0c754e69a823f2024",
+    "url": "/EvaBotApp/static/css/2.6214b97d.chunk.css"
+  },
+  {
+    "revision": "92f0c754e69a823f2024",
+    "url": "/EvaBotApp/static/js/2.cf7a565a.chunk.js"
+  },
+  {
+    "revision": "914b984f7fe12d79593c29d39f42bc11",
+    "url": "/EvaBotApp/static/media/persik.914b984f.png"
+  },
+  {
+    "revision": "e0104accba12cd9d2d624436b9585fef",
+    "url": "/EvaBotApp/index.html"
+  }
+];
